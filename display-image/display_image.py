520d348a8549aba13a590d1ee28138cd8490425a
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-04-27 18:15:10 +0530

    added size

--- display-image/display_image.py
+++ display-image/display_image.py
@@ -1,6 +1,9 @@
 import cv2
 # imread function reads the image specified
 img=cv2.imread('dispimg.jpg')
+## read size of the image
+(h,w)=img.shape[:2]
 # imshow displays the image
+print("The size of the image is",h,"x",w)
 cv2.imshow("Image",img)
 cv2.waitKey(0)
