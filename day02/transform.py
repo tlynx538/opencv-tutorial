e977878c34edc3c58be37a886a0c548c87f56d84
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-04-28 20:18:49 +0530

    adapted code from pyimagesearch

--- day02/transform.py
+++ day02/transform.py
@@ -1,66 +1,65 @@
-''' Building a 4 POINT perspective transform
-    This method can help us get a birds eye 
-    view of the image specified.
-    It is useful in finding the distance between
-    two objects if one object is behind another
-'''
+# import the necessary packages
 import numpy as np
-import cv2 
+import cv2
 
-'''
-  we shall initially define order_points method
-  that accepts the parameter pts which is a list 
-  of coordinates of the rectangle. 
-'''
 def order_points(pts):
-    #we shall initialize the rectangle with 
-    # 0's containing 4 rows and 2 columns
-    rect=np.zeros((4,2),dtype='float32')
-    
-    s=pts.sum(axis=1)
-    #rect[0] i.e the top left will have lowest sum
-    rect[0]=pts[np.argmin(s)]
-    #rect[1] i.e the bottom right will have highest sum
-    rect[2]=pts[np.argmax(s)]
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
 
-    diff=np.diff(pts,axis=1)
-    #rect[1] i.e the top right will have lowest difference
-    rect[1]=pts[np.argmin(diff)]
-    #rect[3] i.e the bottom left will have highest difference
-    rect[3]=pts[np.argmax(diff)]
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
 
-    # returns the rectangle itself
-    return rect
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
 
-def fourptransf(img,pts):
-    #consistent ordering of points by order_point method
-    rect=order_points(pts)
-    (tl,tr,bl,br)=rect
+	# return the ordered coordinates
+	return rect
 
-    # compute max width of image 
-    widthA=np.sqrt(((br[0]-bl[0])**2))+((br[1]-bl[1])**2)
-    widthB=np.sqrt(((tr[0]-tl[0])**2))+((tr[1]-tl[1])**2)
-    maxWidth=max(int(widthA),int(widthB))
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
 
-    #compute max height of image
-    heightA=np.sqrt(((tr[0]-br[0])**2))+((tr[1]-br[1])**2)
-    heightB=np.sqrt(((tl[0]-bl[0])**2))+((tl[1]-bl[1])**2)
-    maxHeight=max(int(heightA),int(heightB))
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
 
-    #construct the birds eye view of the image
-    dst=np.array([
-        [0,0],
-        [maxWidth-1,0],
-        [maxWidth-1,maxHeight-1]
-        [0,maxHeight-1]],dtype='float32')
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
 
-    #computing the perspective transform matrix
-    # perspectiveTransform Calculates a perspective transform from four pairs
-    #  of the corresponding points.
-    m = cv2.perspectiveTransform(rect,dst)
-    # warpPerspective Applies a perspective transformation to an image.
-    warp=cv2.warpPerspective(rect,m,(maxWidth,maxHeight))
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
 
-    #return warped image
-    return warp    
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+	# return the warped image
+	return warped   
 
